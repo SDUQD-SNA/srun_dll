ad62659c457958beb295eed2e6779bcd5127adc1
Forgot-Dream <56014859+Forgot-Dream@users.noreply.github.com>
2024-05-10 22:29:07 +0800

    style: format the code style bump: v0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,30 @@
-use std::ffi::{c_char, CStr};
 use srun::{SrunClient, User};
+use std::ffi::{c_char, CStr};
 
 #[no_mangle]
-pub extern "C" fn ex_logout(auth_server_pointer: *const c_char, username_pointer: *const c_char, ip_pointer: *const c_char) -> bool {
-    if auth_server_pointer.is_null() || username_pointer.is_null() || ip_pointer.is_null() { return false; }
+pub extern "C" fn ex_logout(
+    auth_server_pointer: *const c_char,
+    username_pointer: *const c_char,
+    ip_pointer: *const c_char,
+) -> bool {
+    if auth_server_pointer.is_null() || username_pointer.is_null() || ip_pointer.is_null() {
+        return false;
+    }
 
-    let auth_server: String = unsafe { CStr::from_ptr(auth_server_pointer).to_str().expect("Can not read string argument.").to_string() };
-    let username: String = unsafe { CStr::from_ptr(username_pointer) }.to_str().expect("Can not read string argument.").to_string();
-    let ip: String = unsafe { CStr::from_ptr(ip_pointer) }.to_str().expect("Can not read string argument.").to_string();
+    let auth_server: String = unsafe { CStr::from_ptr(auth_server_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
+    let username: String = unsafe { CStr::from_ptr(username_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
+    let ip: String = unsafe { CStr::from_ptr(ip_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
 
-    println!("auth_server: {} username: {} ip: {}",auth_server,username,ip);
-
-    let mut client = SrunClient::new_for_logout(&auth_server, &username, &ip)
-        .set_detect_ip(true);
+    let mut client = SrunClient::new_for_logout(&auth_server, &username, &ip).set_detect_ip(true);
 
     if let Err(e) = client.logout() {
         println!("Error :{}", e);
@@ -22,15 +34,36 @@ pub extern "C" fn ex_logout(auth_server_pointer: *const c_char, username_pointer
 }
 
 #[no_mangle]
-pub extern "C" fn ex_login(auth_server_pointer: *const c_char, username_pointer: *const c_char,password_pointer:*const c_char, ip_pointer: *const c_char) -> bool {
-    if auth_server_pointer.is_null() || username_pointer.is_null() || ip_pointer.is_null() || password_pointer.is_null() { return false; }
+pub extern "C" fn ex_login(
+    auth_server_pointer: *const c_char,
+    username_pointer: *const c_char,
+    password_pointer: *const c_char,
+    ip_pointer: *const c_char,
+) -> bool {
+    if auth_server_pointer.is_null()
+        || username_pointer.is_null()
+        || ip_pointer.is_null()
+        || password_pointer.is_null()
+    {
+        return false;
+    }
 
-    let auth_server: String = unsafe { CStr::from_ptr(auth_server_pointer).to_str().expect("Can not read string argument.").to_string() };
-    let username: String = unsafe { CStr::from_ptr(username_pointer) }.to_str().expect("Can not read string argument.").to_string();
-    let password: String = unsafe { CStr::from_ptr(password_pointer) }.to_str().expect("Can not read string argument.").to_string();
-    let ip: String = unsafe { CStr::from_ptr(ip_pointer) }.to_str().expect("Can not read string argument.").to_string();
-
-    println!("auth_server: {} username: {} ip: {}",auth_server,username,ip);
+    let auth_server: String = unsafe { CStr::from_ptr(auth_server_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
+    let username: String = unsafe { CStr::from_ptr(username_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
+    let password: String = unsafe { CStr::from_ptr(password_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
+    let ip: String = unsafe { CStr::from_ptr(ip_pointer) }
+        .to_str()
+        .expect("Can not read string argument.")
+        .to_string();
 
     let user = User {
         username,
@@ -41,8 +74,7 @@ pub extern "C" fn ex_login(auth_server_pointer: *const c_char, username_pointer:
 
     println!("login user: {:#?}", user);
 
-    let mut client = SrunClient::new_from_user(&auth_server, user)
-        .set_detect_ip(true);
+    let mut client = SrunClient::new_from_user(&auth_server, user).set_detect_ip(true);
 
     if let Err(e) = client.login() {
         println!("Error :{}", e);
